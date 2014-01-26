90d1a909a94ab249683b4573398dd24c3ae74b75
Alain <alain@serial-labs.com>
2014-01-26 11:09:58 +0100

    Logging features and tests
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs.Logging.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerialLabs.Logging.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7e541fb1-cc25-40e4-a8d2-df8942e4651b")]
+
