d0221bb8cea8149ea89d72f845f5fe728f0adc14
Alain <alain@serial-labs.com>
2014-01-28 19:37:28 +0100

    RouteDataValue Resolver
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerialLabs.Web")]
+[assembly: AssemblyDescription("This library contains Web related reusable components")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b8033391-8ace-42f1-8755-5ce95b0bfdc1")]
---
+++ Source/SerialLabs.Web/RouteDataValueResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+
+namespace SerialLabs.Web
+{
+    /// <summary>
+    /// Helper class to resolve route data values
+    /// </summary>
+    public static class RouteDataValueResolver
+    {
+        /// <summary>
+        /// Find the value corresponding to the given key name.
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">When key does not exist</exception>
+        public static string Resolve(HttpContextBase context, string keyName)
+        {
+            Guard.ArgumentNotNull(context, "context");
+            Guard.ArgumentNotNullOrWhiteSpace(keyName, "keyName");
+
+            if (context.Request == null)
+                throw new InvalidOperationException("HttpContextBase Request is null");
+            if (context.Request.RequestContext == null)
+                throw new InvalidOperationException("HttpContextBase Request RequestContext is null");
+            if (!context.Request.RequestContext.RouteData.Values.ContainsKey(keyName))
+                return null;
+            return context.Request.RequestContext.RouteData.Values[keyName].ToString();
+        }
+    }
+}
