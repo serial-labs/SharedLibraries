d7c03a43fd6e5e41f120bbe135a620c589c3fc69
Alain <alain@serial-labs.com>
2014-01-29 19:58:38 +0100

    Data.Entity Project
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs.Data.Entity/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerialLabs.Data.Entity")]
+[assembly: AssemblyDescription("serial-labs' EntityFramework components")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("267afc91-403b-45ff-8f63-33f729117600")]
