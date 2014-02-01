b94b209bd1433f7d10ce72925bd9ddb2884ec42d
Alain <alain@serial-labs.com>
2014-02-01 08:32:54 +0100

    Refactoring
    Signed-off-by: Alain <alain@serial-labs.com>

--- Source/SerialLabs.Web/RouteDataValueResolver.cs
+++ Source/SerialLabs.Web/RouteDataValueResolver.cs
@@ -27,5 +27,25 @@ namespace SerialLabs.Web
                 return null;
             return context.Request.RequestContext.RouteData.Values[keyName].ToString();
         }
+        /// <summary>
+        /// Find the value corresponding to the given key name.
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">When key does not exist</exception>
+        public static string Resolve(string keyName)
+        {
+            Guard.ArgumentNotNullOrWhiteSpace(keyName, "keyName");
+
+            if (HttpContext.Current == null)
+                throw new InvalidOperationException("Http Context is null");
+            if (HttpContext.Current.Request == null)
+                throw new InvalidOperationException("Http Context Request is null");
+            if (HttpContext.Current.Request.RequestContext == null)
+                throw new InvalidOperationException("Http Context Request RequestContext is null");
+            if (!HttpContext.Current.Request.RequestContext.RouteData.Values.ContainsKey(keyName))
+                return null;
+            return HttpContext.Current.Request.RequestContext.RouteData.Values[keyName].ToString();
+        }
     }
 }
---
+++ Source/SerialLabs/Contracts/IFactory.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SerialLabs
+{
+    /// <summary>
+    /// Factory Interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IFactory<T>
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="{T}"/>
+        /// </summary>
+        /// <returns></returns>
+        T Create();
+    }
+}
