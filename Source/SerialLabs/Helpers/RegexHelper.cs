c61fd83bb9189675a86a72ee684970d1f5746c6d
Alain <alain@serial-labs.com>
2014-02-04 19:23:44 +0100

    Regex Helper
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs/Helpers/RegexHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SerialLabs
+{
+    /// <summary>
+    /// Provides regex utilities
+    /// </summary>
+    public static class RegexHelper
+    {
+        #region Public Methods
+        /// <summary>
+        /// SHA-256 Regex pattern
+        /// </summary>
+        public const string SHA256Pattern = @"^[a-fA-F\d]{64}$";
+        /// <summary>
+        /// MD5 Regex pattern
+        /// </summary>
+        public const string MD5Pattern = @"^[a-fA-F\d]{32}$";
+        /// <summary>
+        /// Secret Key Regex pattern
+        /// </summary>
+        public const string SecretKeyPattern = @"^[a-zA-Z\d]{16}$";
+        /// <summary>
+        /// Email Regex pattern
+        /// </summary>
+        public const string EmailPattern =
+                @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+        /// <summary>
+        /// Url Regex pattern
+        /// </summary>
+        public const string UrlPattern = @"^(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?$";
+        /// <summary>
+        /// Access Code pattern
+        /// </summary>
+        public const string AccessCodePattern = @"^[a-zA-Z0-9_\-\.]{10}$";
+        /// <summary>
+        /// Returns true if the given value is an SHA-256 hash
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsSHA256Hash(string value)
+        {
+            return IsPatternMatch(value, SHA256Pattern);
+        }
+        /// <summary>
+        /// Returns true if the given value is a MD5 hash
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsMD5Hash(string value)
+        {
+            return IsPatternMatch(value, MD5Pattern);
+        }
+        /// <summary>
+        /// Returns true if the given value is a secret key
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsSecretKey(string value)
+        {
+            return IsPatternMatch(value, SecretKeyPattern);
+        }
+        /// <summary>
+        /// Returns true if the given value is an email
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsEmail(string value)
+        {
+            return IsPatternMatch(value, EmailPattern);
+        }
+        /// <summary>
+        /// Returns true if the given value is a valid url
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsUrl(string value)
+        {
+            return IsPatternMatch(value, UrlPattern);
+            //return Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute);
+        }
+        /// <summary>
+        /// Returns true if the given value is an access code
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsAccessCode(string value)
+        {
+            return IsPatternMatch(value, AccessCodePattern);
+        }
+        #endregion
+
+        #region Private Methods
+        private static bool IsPatternMatch(string value, string pattern, RegexOptions regexOptions = RegexOptions.IgnoreCase | RegexOptions.Compiled)
+        {
+            if (String.IsNullOrWhiteSpace(value))
+                return false;
+            if (String.IsNullOrWhiteSpace(pattern))
+                throw new ArgumentNullException("pattern");
+            return Regex.IsMatch(value, pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        }
+        #endregion
+    }
+}
---
+++ Source/UnitTests/SerialLabs.Tests/Helpers/RegexHelperTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SerialLabs.Tests
+{
+    [TestClass]
+    public class RegexHelperTest
+    {
+        [TestMethod]
+        public void IsSHA256Test()
+        {
+            Assert.IsTrue(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.Empty.ToString())));
+            Assert.IsTrue(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.NewGuid().ToString())));
+            Assert.IsFalse(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.NewGuid().ToString()).Substring(0, 31)));
+            Assert.IsFalse(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.NewGuid().ToString()).Substring(0, 63) + "@"));
+        }
+        [TestMethod]
+        public void IsSecretKeyTest()
+        {
+            Assert.IsTrue(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.Empty.ToString())));
+            Assert.IsTrue(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.NewGuid().ToString())));
+            Assert.IsFalse(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.NewGuid().ToString()).Substring(0, 12)));
+            Assert.IsFalse(RegexHelper.IsSHA256Hash(CryptographyHelper.ComputeSHA256Hash(Guid.NewGuid().ToString()).Substring(0, 15) + "@"));
+        }
+        [TestMethod]
+        public void IsEmailTest()
+        {
+            Assert.IsTrue(RegexHelper.IsEmail("b172f15bac94d30fde90@serial-labs.com"));
+
+        }
+        [TestMethod]
+        public void IsAccessCodeTest()
+        {
+            Assert.IsFalse(RegexHelper.IsAccessCode("12345678901")); //  11 chars
+            Assert.IsFalse(RegexHelper.IsAccessCode("123456789@")); //   Unauthorized char
+            Assert.IsFalse(RegexHelper.IsAccessCode("123456789é")); //   Unauthorized char
+
+            Assert.IsTrue(RegexHelper.IsAccessCode("1234567890"));
+            Assert.IsTrue(RegexHelper.IsAccessCode("123.456.78"));
+            Assert.IsTrue(RegexHelper.IsAccessCode("123-456-78"));
+        }
+        [TestMethod]
+        public void IsUrlTest()
+        {
+            Assert.IsTrue(RegexHelper.IsUrl("http://www.domain.com/page.html"));
+            Assert.IsTrue(RegexHelper.IsUrl("https://www.domain.com/page.html"));
+            Assert.IsTrue(RegexHelper.IsUrl("ftp://www.domain.com/page.html"));
+            Assert.IsTrue(RegexHelper.IsUrl("http://domain.com/?query=a"));
+            Assert.IsTrue(RegexHelper.IsUrl("http://www.domain.com/page#result"));
+            Assert.IsTrue(RegexHelper.IsUrl("http://www.domain.com/page.html/"));
+            Assert.IsTrue(RegexHelper.IsUrl("http://subdomain.domain.com/page.html"));
+            Assert.IsTrue(RegexHelper.IsUrl("http://www.domain.com/1234,345"));
+            Assert.IsTrue(RegexHelper.IsUrl("http://127.0.0.1:8090/"));
+
+            Assert.IsFalse(RegexHelper.IsUrl("xtp://domain.com"));
+            Assert.IsFalse(RegexHelper.IsUrl("http://localhost/")); // Should use 127.0.0.1
+        }
+    }
+}
