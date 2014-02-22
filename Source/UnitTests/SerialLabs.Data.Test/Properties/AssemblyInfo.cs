557fa6dcf56b86a343db0ebfb5e564568a537b89
Alain <alain@serial-labs.com>
2014-02-23 00:16:16 +0100

    Cleaning
    Signed-off-by: Alain <alain@serial-labs.com>

--- Source/UnitTests/SerialLabs.Data.Test/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SerialLabs.Data.Test")]
-[assembly: AssemblyDescription("Unit Test Suit for SerialLabs.Data")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3154a812-d1e5-42d6-8b7c-fa1dcec34f6a")]
