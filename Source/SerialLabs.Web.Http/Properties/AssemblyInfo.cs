19dee69e801bae9f737857b65998678e247e9fb1
Alain <alain@serial-labs.com>
2014-02-24 22:09:53 +0100

    Added projects Web, Http
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs.Web.Http/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerialLabs.Web.Http")]
+[assembly: AssemblyDescription("Web Http (api)")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6300e442-de64-4c15-bc9b-3846227b9e9b")]
---
+++ Source/SerialLabs.Web.Mvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerialLabs.Web.Mvc")]
+[assembly: AssemblyDescription("SerialLbas Web Mvc features")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d5ca9c09-c567-4f17-ae33-3b2d7a50ed8a")]
