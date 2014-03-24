0713f438a152e595fce7a22f7c7fcf13034a8ad9
Alain <alain@serial-labs.com>
2014-03-24 15:18:56 +0100

    Clean up
    Signed-off-by: Alain <alain@serial-labs.com>

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("0.1.2.1")]
-[assembly: AssemblyFileVersion("0.1.2.1")]
-[assembly: AssemblyInformationalVersion("1.2.1-alpha")]
+[assembly: AssemblyVersion("0.1.2.2")]
+[assembly: AssemblyFileVersion("0.1.2.2")]
+[assembly: AssemblyInformationalVersion("1.2.2-alpha")]
