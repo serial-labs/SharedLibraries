253bd666d8fc80b3276bc7f700962bf17d365c0a
Alain <alain@serial-labs.com>
2014-03-27 14:13:28 +0100

    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs.Web/Monitoring/MonitoringBatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SerialLabs.Web.Monitoring
+{
+    /// <summary>
+    /// Encapsulate the status check results
+    /// </summary>
+    public class MonitoringJobBatch
+    {
+        /// <summary>
+        /// The name is this set of status checks
+        /// </summary>
+        public string Label { get; set; }
+        public IEnumerable<MonitoringJob> JobList { get; set; }
+
+        public static MonitoringJobBatch Create(string label, IEnumerable<MonitoringJob> jobs)
+        {
+            return new MonitoringJobBatch
+            {
+                Label = label,
+                JobList = new List<MonitoringJob>(jobs)
+            };
+        }
+    }
+}
