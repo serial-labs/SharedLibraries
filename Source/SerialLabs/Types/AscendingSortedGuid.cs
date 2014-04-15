a48f65da00661a72c91b3b665dcc875ab2201614
Alain <alain@serial-labs.com>
2014-04-15 16:02:12 +0200

    Equalities and comparisons
    Signed-off-by: Alain <alain@serial-labs.com>

--- Source/SerialLabs/Types/AscendingSortedGuid.cs
+++ Source/SerialLabs/Types/AscendingSortedGuid.cs
@@ -4,21 +4,46 @@ using System.Globalization;
 namespace SerialLabs.Data
 {
     /// <summary>
-    /// A sorted guid with a lexicographic order based on its natural timestamp value.
+    /// A sorted Guid
+    /// This is a composite guid which is associated with a time stamp.
+    /// This time stamp helps to order the guid in a table storage.
     /// </summary>
     public struct AscendingSortedGuid
     {
         private const char Separator = '_';
 
         /// <summary>
+        /// A read-only instance of the AscendingSortedGuid structure whose value is all zeros.
+        /// </summary>
+        public static readonly AscendingSortedGuid Empty;
+
+        static AscendingSortedGuid()
+        {
+            AscendingSortedGuid.Empty = new AscendingSortedGuid();
+        }
+
+        /// <summary>
         /// TimeStamp
         /// </summary>
-        public DateTimeOffset Timestamp { get; set; }
+        public DateTimeOffset Timestamp;
 
         /// <summary>
         /// Guid
         /// </summary>
-        public Guid Guid { get; set; }
+        public Guid Guid;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="AscendingSortedGuid"/>
+        /// </summary>
+        /// <param name="timestamp"></param>
+        /// <param name="guid"></param>
+        public AscendingSortedGuid(DateTimeOffset timestamp, Guid guid)
+        {
+            Guard.ArgumentNotNull(timestamp, "timestamp");
+            Guard.ArgumentNotNull(guid, "guid");
+            this.Timestamp = timestamp;
+            this.Guid = guid;
+        }
 
         /// <summary>
         /// Parse a string value into an <see cref="AscendingSortedGuid"/>
@@ -46,20 +71,6 @@ namespace SerialLabs.Data
         }
 
         /// <summary>
-        /// Creates a new <see cref="AscendingSortedGuid"/>
-        /// </summary>
-        /// <returns></returns>
-        public static AscendingSortedGuid NewSortedGuid()
-        {
-            AscendingSortedGuid result = new AscendingSortedGuid();
-
-            result.Guid = Guid.NewGuid();
-            result.Timestamp = DateTimeOffset.UtcNow;
-
-            return result;
-        }
-
-        /// <summary>
         /// Try to parse a string value into a <see cref="AscendingSortedGuid"/>
         /// </summary>
         /// <param name="id"></param>
@@ -82,12 +93,129 @@ namespace SerialLabs.Data
         }
 
         /// <summary>
+        /// Creates a new <see cref="AscendingSortedGuid"/>
+        /// </summary>
+        /// <returns></returns>
+        public static AscendingSortedGuid NewSortedGuid()
+        {
+            AscendingSortedGuid result = new AscendingSortedGuid();
+
+            result.Guid = Guid.NewGuid();
+            result.Timestamp = DateTimeOffset.UtcNow;
+
+            return result;
+        }
+
+
+
+        /// <summary>
         /// Returns the string representation of the <see cref="AscendingSortedGuid"/>
         /// </summary>
         /// <returns></returns>
         public override string ToString()
         {
             return String.Format(CultureInfo.InvariantCulture, "{0:D19}{1}{2:N}", Timestamp.Ticks, Separator, Guid);
+        }
+
+        /// <summary>
+        /// Gets hash code
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Timestamp.GetHashCode() | Guid.GetHashCode();
+        }
+
+        /// <summary>
+        /// Checks equality
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is DescendingSortedGuid))
+                return false;
+            return Equals((DescendingSortedGuid)obj);
+        }
+
+        /// <summary>
+        /// Returns true if the given <see cref="DescendingSortedGuid"/> is equal to the given instance
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(DescendingSortedGuid other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (this.Timestamp != other.Timestamp)
+                return false;
+            if (this.Guid != other.Guid)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Equality operator overloading
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static bool operator ==(AscendingSortedGuid value1, AscendingSortedGuid value2)
+        {
+            if (value1.Timestamp != value2.Timestamp)
+            {
+                return false;
+            }
+            if (value1.Guid != value2.Guid)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Equality operator overloading
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static bool operator !=(AscendingSortedGuid value1, AscendingSortedGuid value2)
+        {
+            return !(value1 == value2);
+        }
+
+        /// <summary>
+        /// Compares the given value to the current structure
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int CompareTo(object value)
+        {
+            if (value == null)
+                return -1;
+
+            Guard.IsInstanceOfType(typeof(AscendingSortedGuid), value, "value");
+
+            return CompareTo((AscendingSortedGuid)value);
+
+        }
+
+        /// <summary>
+        /// Compares the given value to the current structure
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(AscendingSortedGuid other)
+        {
+            if (this.Timestamp < other.Timestamp)
+                return -1;
+            if (this.Timestamp > other.Timestamp)
+                return 1;
+            // Timestamp are equal, check guid now
+            return this.Guid.CompareTo(other.Guid);
         }
     }
 }
--- Source/SerialLabs/Types/DescendingSortedGuid.cs
+++ Source/SerialLabs/Types/DescendingSortedGuid.cs
@@ -3,21 +3,46 @@
 namespace SerialLabs.Data
 {
     /// <summary>
-    /// A sorted guid with a lexicographic order based on its inverted timestamp value.
+    /// A sorted Guid
+    /// This is a composite guid which is associated with a time stamp.
+    /// This time stamp helps to order the guid in a table storage.
     /// </summary>
-    public struct DescendingSortedGuid
+    public struct DescendingSortedGuid : IComparable, IComparable<DescendingSortedGuid>, IEquatable<DescendingSortedGuid>
     {
         private const char Separator = '_';
 
         /// <summary>
+        /// A read-only instance of the AscendingSortedGuidDescendingSortedGuid structure whose value is all zeros.
+        /// </summary>
+        public readonly static DescendingSortedGuid Empty;
+
+        static DescendingSortedGuid()
+        {
+            DescendingSortedGuid.Empty = new DescendingSortedGuid();
+        }
+
+        /// <summary>
         /// TimeStamp
         /// </summary>
-        public DateTimeOffset Timestamp { get; set; }
+        public DateTimeOffset Timestamp;
 
         /// <summary>
         /// Guid
         /// </summary>
-        public Guid Guid { get; set; }
+        public Guid Guid;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="DescendingSortedGuid"/>
+        /// </summary>
+        /// <param name="timestamp"></param>
+        /// <param name="guid"></param>
+        public DescendingSortedGuid(DateTimeOffset timestamp, Guid guid)
+        {
+            Guard.ArgumentNotNull(timestamp, "timestamp");
+            Guard.ArgumentNotNull(guid, "guid");
+            this.Timestamp = timestamp;
+            this.Guid = guid;
+        }
 
         /// <summary>
         /// Parse a string value into a <see cref="DescendingSortedGuid"/>
@@ -86,6 +111,107 @@ namespace SerialLabs.Data
         public override string ToString()
         {
             return String.Format("{0:D19}{1}{2:N}", DateTime.MaxValue.Ticks - Timestamp.Ticks, Separator, Guid);
+        }
+
+        /// <summary>
+        /// Gets hash code
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Timestamp.GetHashCode() | Guid.GetHashCode();
+        }
+
+        /// <summary>
+        /// Checks equality
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is DescendingSortedGuid))
+                return false;
+            return Equals((DescendingSortedGuid)obj);
+        }
+
+        /// <summary>
+        /// Returns true if the given <see cref="DescendingSortedGuid"/> is equal to the given instance
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(DescendingSortedGuid other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (this.Timestamp != other.Timestamp)
+                return false;
+            if (this.Guid != other.Guid)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Equality operator overloading
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static bool operator ==(DescendingSortedGuid value1, DescendingSortedGuid value2)
+        {
+            if (value1.Timestamp != value2.Timestamp)
+            {
+                return false;
+            }
+            if (value1.Guid != value2.Guid)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Equality operator overloading
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static bool operator !=(DescendingSortedGuid value1, DescendingSortedGuid value2)
+        {
+            return !(value1 == value2);
+        }
+
+        /// <summary>
+        /// Compares the given value to the current structure
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int CompareTo(object value)
+        {
+            if (value == null)
+                return -1;
+
+            Guard.IsInstanceOfType(typeof(DescendingSortedGuid), value, "value");
+
+            return CompareTo((DescendingSortedGuid)value);
+
+        }
+
+        /// <summary>
+        /// Compares the given value to the current structure
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(DescendingSortedGuid other)
+        {
+            if (this.Timestamp < other.Timestamp)
+                return -1;
+            if (this.Timestamp > other.Timestamp)
+                return 1;
+            // Timestamp are equal, check guid now
+            return this.Guid.CompareTo(other.Guid);
         }
     }
 }
