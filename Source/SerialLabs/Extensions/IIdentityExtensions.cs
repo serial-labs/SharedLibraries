f9a8bb8ca8f577f10975baac610f1e8753e1afa1
Alain <alain@serial-labs.com>
2014-05-01 23:51:16 +0200

    Casting shortcuts for Claims... Security interfaces
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs/Extensions/IIdentityExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace SerialLabs
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="IIdentity"/> interface.
+    /// </summary>
+    public static class IIdentityExtensions
+    {
+        /// <summary>
+        /// Returns the current <see cref="IIdentity"/> as a <see cref="ClaimsIdentity"/>
+        /// </summary>
+        /// <param name="identity"></param>
+        /// <returns></returns>
+        public static ClaimsIdentity AsClaimsIdentity(this IIdentity identity)
+        {
+            return identity as ClaimsIdentity;
+        }
+    }
+}
---
+++ Source/SerialLabs/Extensions/IPrincipalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace SerialLabs
+{
+    /// <summary>
+    /// Provides extensions methods for the <see cref="IPrincipal"/> interface
+    /// </summary>
+    public static class IPrincipalExtensions
+    {
+        /// <summary>
+        /// Returns the current <see cref="IPrincipal"/> as a <see cref="ClaimsPrincipal"/>
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public static ClaimsPrincipal AsClaimsPrincipal(this IPrincipal principal)
+        {
+            return principal as ClaimsPrincipal;
+        }
+    }
+}
