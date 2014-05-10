3d4637191aa1ebf469fe060949e3f2c88bcf4215
Alain <alain@serial-labs.com>
2014-05-10 20:06:24 +0200

    Added logo to Nuspec
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs.Web.Mvc/SimpleKeyAuthorizationAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace SerialLabs.Web.Mvc
+{
+    /// <summary>
+    /// A simple authorize attribute which enforce a query string key authorization
+    /// </summary>
+    public class SimpleKeyAuthorizationAttribute : AuthorizeAttribute
+    {
+        private string _key;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="SimpleKeyAuthorizationAttribute"/>
+        /// </summary>
+        /// <param name="appSettingName"></param>
+        public SimpleKeyAuthorizationAttribute(string appSettingName)
+        {
+            Guard.ArgumentNotNullOrWhiteSpace(appSettingName, "appSettingName");
+            _key = ConfigurationManager.AppSettings[appSettingName];
+        }
+
+        /// <summary>
+        /// Authorize
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (String.IsNullOrWhiteSpace(_key))
+                return;
+
+            string[] values = filterContext.HttpContext.Request.QueryString.GetValues("key");
+            if (values == null || values.Length == 0 || values[0].ToString() != _key)
+                filterContext.Result = new HttpNotFoundResult();
+        }
+    }
+}
