a1028fb0edb3310b2ae3545e7f609ead5b84b1cd
Alain <alain@serial-labs.com>
2014-05-13 23:06:06 +0200

    Added LowerCamelCaseContractResolver for JSON serialization
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs/Serialization/LowerCamelCaseContractResolver.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+
+namespace SerialLabs.Serialization
+{
+    /// <summary>
+    /// Resolves properties in lower camel case (lowerCamelCase)
+    /// </summary>
+    public class LowerCamelCaseContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        /// <summary>
+        /// Returns the property names as lower camel cased (lowerCamelCase)
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            string result = base.ResolvePropertyName(propertyName);
+            return String.IsNullOrEmpty(result) ? result : result[0].ToString().ToLowerInvariant() + result.Substring(1, result.Length - 1);
+        }
+    }
+}
