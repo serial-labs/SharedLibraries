8fc935a403d033cc838e429ff0dcf1b1b6416265
Alain <alain@serial-labs.com>
2014-06-04 11:59:37 +0200

    Assembly helper provides utility methods to manipulate Assemblies at runtime.
    Signed-off-by: Alain <alain@serial-labs.com>

---
+++ Source/SerialLabs/Helpers/AssemblyHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace SerialLabs
+{
+    public static class AssemblyHelper
+    {
+        public static string GetInformationalVersion()
+        {
+            return FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+        }
+
+        public static string GetInformationalVersion(Assembly assembly)
+        {
+            return FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+        }
+    }
+}
