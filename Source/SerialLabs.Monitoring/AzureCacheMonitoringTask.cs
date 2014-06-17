52d140d23a24f98b629e45beada7b3ee3dd7d838
Alain <alain@serial-labs.com>
2014-06-17 16:38:06 +0200

    Cache rework Signed-off-by: Alain <alain@serial-labs.com>

--- Source/SerialLabs.Monitoring/AzureCacheMonitoringTask.cs
+++ Source/SerialLabs.Monitoring/AzureCacheMonitoringTask.cs
@@ -6,6 +6,7 @@ namespace SerialLabs.Monitoring
 {
     public class AzureCacheMonitoringTask : MonitoringTask
     {
+        protected DataCache Cache { get; private set; }
         protected string CacheRegion { get; private set; }
         protected string CacheKey { get; private set; }
 
@@ -14,29 +15,34 @@ namespace SerialLabs.Monitoring
         { }
 
         public AzureCacheMonitoringTask(string label, string cacheRegion, string cacheKey)
-            : base(label)
+            : this(label, new DataCache(), cacheRegion, cacheKey)
+        { }
+
+        public AzureCacheMonitoringTask(string label, DataCache cache, string cacheRegion, string cacheKey)
         {
+            Guard.ArgumentNotNull(cache, "cache");
+            Cache = cache;
+
             Guard.ArgumentNotNullOrWhiteSpace(cacheRegion, "cacheRegion");
             CacheRegion = cacheRegion;
+
             Guard.ArgumentNotNullOrWhiteSpace(cacheKey, "cacheKey");
             CacheKey = cacheKey;
         }
 
         protected override void ExecuteCore()
         {
-            DataCache cache = new DataCache();
-            cache.CreateRegion(CacheRegion);
-            cache.Put(CacheKey, Guid.NewGuid().ToString(), CacheRegion);
-            string result = (string)cache.Get(CacheKey, CacheRegion);
+            Cache.CreateRegion(CacheRegion);
+            Cache.Put(CacheKey, Guid.NewGuid().ToString(), CacheRegion);
+            string result = (string)Cache.Get(CacheKey, CacheRegion);
         }
 
         protected override Task ExecuteCoreAsync()
         {
-            DataCache cache = new DataCache();
-            cache.CreateRegion(CacheRegion);
-            cache.Put(CacheKey, Guid.NewGuid().ToString(), CacheRegion);
-            string result = (string)cache.Get(CacheKey, CacheRegion);
-            return Task.FromResult<string>((string)cache.Get(CacheKey));
+            Cache.CreateRegion(CacheRegion);
+            Cache.Put(CacheKey, Guid.NewGuid().ToString(), CacheRegion);
+            string result = (string)Cache.Get(CacheKey, CacheRegion);
+            return Task.FromResult<string>((string)Cache.Get(CacheKey));
         }
     }
 }
